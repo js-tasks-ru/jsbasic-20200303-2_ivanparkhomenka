1107a222011bdca3567552c40eb50fd5fc2e731a
Ivan Parkhomenka <i.parkhomenka@ozitag.com>
2020-03-17 19:26:23 +0300

    add sumSalary function

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,13 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
 
+function sumSalary(salaries) {
+  let sum = 0;
+  for (let i in salaries) {
+    if (typeof salaries[i] === "number") {
+      sum += salaries[i];
+    }
+  }
+  return sum;
 }
