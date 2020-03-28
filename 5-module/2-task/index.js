67e7cdb0288bc2d770a50a66d8fdb95111d59dcc
Ivan Parkhomenka <i.parkhomenka@ozitag.com>
2020-03-28 18:19:55 +0300

    add SortableTable  function

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -17,7 +17,32 @@ function SortableTable(items) {
   /**
    * @property {Element} - обязательное свойство, которое ссылается на элемент <table>
    */
+  let sortedData;
   this.el = document.createElement('table');
+
+  let initialHTML = `
+<thead>
+  <tr>
+     <td>Name</td>
+     <td>Age</td>
+     <td>Salary</td>
+     <td>City</td>
+  </tr>
+</thead>`;
+
+  this.el.innerHTML = initialHTML;
+
+  items.map((item) => {
+    let tbody = document.createElement('tbody');
+    tbody.innerHTML += `<tr>
+   <td>${item.name}</td>
+   <td>${item.age}</td>
+   <td>${item.salary}</td>
+   <td>${item.city}</td>
+</tr>`;
+    this.el.appendChild(tbody);
+  });
+
 
   /**
    * Метод выполняет сортировку таблицы
@@ -25,5 +50,52 @@ function SortableTable(items) {
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+  this.sort = (column, desc = false) => {
+    this.el.innerHTML = initialHTML;
+
+    if (column === 0 && desc === false) {
+      sortedData = items.sort((prev, next) => prev.name > next.name ? 1 : -1);
+    }
+
+    if (column === 0 && desc === true) {
+      sortedData = items.sort((prev, next) => prev.name < next.name ? 1 : -1);
+    }
+
+    if (column === 3 && desc === false) {
+      sortedData = items.sort((prev, next) => prev.city > next.city ? 1 : -1);
+    }
+
+    if (column === 3 && desc === true) {
+      sortedData = items.sort((prev, next) => prev.city < next.city ? 1 : -1);
+    }
+
+    if (column === 2 && desc === false) {
+      sortedData = items.sort((prev, next) => prev.salary - next.salary);
+    }
+
+    if (column === 2 && desc === true) {
+      sortedData = items.sort((prev, next) => next.salary - prev.salary);
+    }
+
+    if (column === 1 && desc === false) {
+      sortedData = items.sort((prev, next) => prev.age - next.age);
+    }
+
+    if (column === 1 && desc === true) {
+      sortedData = items.sort((prev, next) => next.age - prev.age);
+    }
+
+    let tbody = document.createElement('tbody');
+
+    sortedData.map((item) => {
+      tbody.innerHTML += `
+  <tr>
+      <td>${item.name}</td>
+      <td>${item.age}</td>
+      <td>${item.salary}</td>
+      <td>${item.city}</td>
+  </tr>`;
+    });
+    this.el.appendChild(tbody);
+  };
 }
